0b412a58aed99fe3dc9d1aecb62d01dd1780c362
Drairline <Daniil.airlend@gmail.com>
2026-03-04 16:41:47 +0100

    Score_calculator

--- Score_calculator.py
+++ Score_calculator.py
@@ -5,3 +5,17 @@ Created on Mon Mar  2 13:39:22 2026
 @author: 24daai01
 """
 
+dielist = [3,3,4,3,2]
+
+dictlist = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+
+
+for i in dielist:
+    if i in dictlist:
+        dictlist[i] += 1
+
+h = list(dictlist.values())
+
+print(h)
+
+def three_of_a_kind(h):
