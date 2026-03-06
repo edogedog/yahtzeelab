4196ffbf15f7cd8f9b34666735967f42a0dcd9d9
Drairline <Daniil.airlend@gmail.com>
2026-03-06 15:36:58 +0100

    Score_calculator.py
    Vi behöver endast fixa 1 par, 2 par, tretal och fyrtal. Efter det så är alla funktioner klara om vi måste försöka lösa hur vi ska koppla det till vårt main program. Då tärningarna är slump kastade!

--- Score_calculator.py
+++ Score_calculator.py
@@ -5,7 +5,7 @@ Created on Mon Mar  2 13:39:22 2026
 @author: 24daai01
 """
 
-dielist = [1,2,3,4,5,6]
+dielist = [2,3,4,5,5]
 
 dictlist = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
 
@@ -18,7 +18,7 @@ h = list(dictlist.values())
 
 print(h)
 
-#här är en massa fina funktioner av acailable scores vi ska checka om h är, vi ska definera dessa så de checkar för hn, etta pax ja tar three of kind four of kinf o kåååk
+
 def score_ones(h):
     return h[0] * 1
 
@@ -37,57 +37,68 @@ def score_fives(h):
 def score_sixes(h):
     return h[5] * 6
 
-print("Poäng för Ettor:", score_ones(h))  # Förväntad output: 0
-print("Poäng för Tvåor:", score_twos(h))  # Förväntad output: 2 (1 tvåa * 2)
-print("Poäng för Treor:", score_threes(h))  # Förväntad output: 9 (3 treor * 3)
-print("Poäng för Fyror:", score_fours(h))  # Förväntad output: 4 (1 fyra * 4)
-print("Poäng för Femmor:", score_fives(h))  # Förväntad output: 0
-print("Poäng för Sexor:", score_sixes(h))  # Förväntad output: 0
+print("Poäng för Ettor: ", score_ones(h))
+print("Poäng för Tvåor: ", score_twos(h))  
+print("Poäng för Treor: ", score_threes(h)) 
+print("Poäng för Fyror: ", score_fours(h))  
+print("Poäng för Femmor: ", score_fives(h))  
+print("Poäng för Sexor: ", score_sixes(h))  
 
 
-
+"""
 def score_pair(h):
-    result = 0
-    if 2 in histogram:
-        result =  2 * (1 + histogram.index(3)) # 3 * det tärningsvärde som hittades tre av
-    return result
 
 def score_two_pair(h):
 
-
 def score_three_of_a_kind(h):
-    result = 0
-    if 3 in histogram:
-        result =  3 * (1 + histogram.index(3)) # 3 * det tärningsvärde som hittades tre av
-    return result
-    
 
 def score_four_of_a_kind(h):
-    result = 0
-    if 4 in histogram:
-        result =  4 * (1 + histogram.index(3)) # 3 * det tärningsvärde som hittades tre av
-    return result
+    """
 
 def score_low_straight(h):
+    if h[0:5].count(1) == 5 and h[5] == 0:
+        return 15
+    return 0
+        
+print("Poängen för liten stege: ", score_low_straight(h))
 
 def score_high_straight(h):
+    if h[1:6].count(1) == 5 and h[0] == 0:
+        return 20
+    return 0
+
+print("Poäng för stor stege: ", score_high_straight(h))
     
 
 def score_full_house(h):
-    i = h.index(3)
-    j = h.index(2)
-    if i>=0 and j>=0:
+    result = 0
+    if 3 in h and 2 in h:
+        i = h.index(3)
+        j = h.index(2)
         result = 3 * (i+1) + 2 *(j+1)
-        return result
+    return result
     
-# Histogrammet för tärningskast: [6, 6, 6, 3, 3]
-histogram = [0, 0, 1, 2, 0, 3]  # 3 stycken 6:or, 2 stycken 3:or
-print(score_full_house(histogram))  # Förväntad output: 3 * 6 + 2 * 3 = 18 + 6 = 24
+
+print("Poäng för Kåk: ", score_full_house(h))
 
 
 def score_chance(h):
+    result = 0
+    for i in range(len(h)):
+        result += h[i] * (i + 1)
+    return result
+
+print("Poäng för chans: ", score_chance(h))
+    
 
 def score_yatzy(h):
+    if 5 in h:
+        return 50
+    return 0
+    
+print("Poäng för yatzy: ", score_yatzy(h))
+
+
     
 #här gör vi en dictionary grejsimojs som vi sedan kommer gå igenom där vi checkar igenom alla våra funtkioner med en for loop där de kommer va tyyyyp fonånting, nånting in fd: o sen under de typ cat för categori o fun för funktion
 fd = {
@@ -97,10 +108,10 @@ fd = {
     "Fyror": score_fours,
     "Femmor": score_fives,
     "Sexor": score_sixes,
-    "1 par": score_pair,
-    "2 par": score_two_pair,
-    "Tretal": score_three_of_a_kind,
-    "Fyrtal": score_four_of_a_kind,
+#    "1 par": score_pair,
+#    "2 par": score_two_pair,
+#    "Tretal": score_three_of_a_kind,
+#    "Fyrtal": score_four_of_a_kind,
     "Liten stege": score_low_straight,
     "Stor stege": score_high_straight,
     "Kåk": score_full_house,
