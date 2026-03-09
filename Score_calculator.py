1e1d443e2c11dec5e1ec3d5bc867fec65cdf7bbf
edogedog <edgareriksson@icloud.com>
2026-03-09 15:31:31 +0100

    wip/wipfunctionwdict

--- Score_calculator.py
+++ Score_calculator.py
@@ -1,6 +1,4 @@
-@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
--- coding: utf-8 --
 """
 Created on Mon Mar  2 13:39:22 2026
 
@@ -12,7 +10,7 @@ dielist = [2,3,4,5,5]
 
 dictlist = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
 
-@@ -22,84 +22,102 @@
+
 def score_ones(h):
     return h[0] * 1
 
@@ -44,8 +42,8 @@ def score_sixes(h):
 
 def score_pair(h):
     result = 0
-    if 2 in histogram:
-        result =  2 * (1 + histogram.index(3)) # 3 * det tärningsvärde som hittades tre av
+    if 2 in h:
+        result =  2 * (1 + h.index(3)) # 3 * det tärningsvärde som hittades tre av
     return result
     
 
@@ -60,16 +58,16 @@ def score_two_pair(h):
 
 def score_three_of_a_kind(h):
     result = 0
-    if 3 in histogram:
-        result =  3 * (1 + histogram.index(3)) # 3 * det tärningsvärde som hittades tre av
+    if 3 in h:
+        result =  3 * (1 + h.index(3)) # 3 * det tärningsvärde som hittades tre av
     return result
     
 
 def score_four_of_a_kind(h):
     
     result = 0
-    if 4 in histogram:
-        result =  4 * (1 + histogram.index(3)) # 3 * det tärningsvärde som hittades tre av
+    if 4 in h:
+        result =  4 * (1 + h.index(3)) # 3 * det tärningsvärde som hittades tre av
     return result
 
 def score_low_straight(h):
@@ -77,7 +75,7 @@ def score_low_straight(h):
         return 15
     return 0
         
-print("Poängen för liten stege: ", score_low_straight(h))
+#print("Poängen för liten stege: ", score_low_straight(h))
 
 
 def score_high_straight(h):
@@ -85,7 +83,7 @@ def score_high_straight(h):
         return 20
     return 0
 
-print("Poäng för stor stege: ", score_high_straight(h))
+#print("Poäng för stor stege: ", score_high_straight(h))
     
 
 
@@ -99,7 +97,7 @@ def score_full_house(h):
     return result
     
 
-print("Poäng för Kåk: ", score_full_house(h))
+#print("Poäng för Kåk: ", score_full_house(h))
 
 
 
@@ -110,7 +108,7 @@ def score_chance(h):
         result += h[i] (i + 1)
     return result
 
-print("Poäng för chans: ", score_chance(h))
+#print("Poäng för chans: ", score_chance(h))
     
 
 
@@ -119,7 +117,7 @@ def score_yatzy(h):
         return 50
     return 0
     
-print("Poäng för yatzy: ", score_yatzy(h))
+#print("Poäng för yatzy: ", score_yatzy(h))
 
 
     
@@ -147,4 +145,12 @@ fd = {
 
 for key in fd.keys():
     print(key, fd[key]([0,1,1,1,1,1]))    
+
+
+
+
+
+
+
+
 
