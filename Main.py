280a8226069e951422fbe25bd999f30133dd0360
Drairline <Daniil.airlend@gmail.com>
2026-03-09 16:06:08 +0100

    Kopplar ihop score_calculator till main

--- Main.py
+++ Main.py
@@ -6,23 +6,33 @@ Created on Mon Mar  2 13:38:32 2026
 """
 
 from Dicegroup import DiceGroup
+import Score_calculator
 
 if __name__ == "__main__":
-    info = "Tryck 'Enter' för att kasta tärningarna. Välja vilka träningar du vill behålla 1 - 5. Om du vill avsluta skriv 'q': "
+    info = "Tryck 'Enter' för att kasta tärningarna. Välja vilka tärningar du vill behålla (1-5). Om du vill avsluta skriv 'q': "
     dg = DiceGroup()
     cap = 0
     while cap < 3:
         choice = input(info)
+
+        if choice.lower() == 'q':
+            break
+
         if choice != "":
             for ch in choice:
                 dg.hold(int(ch)-1)
-                print(dg)
+            print(dg)
+
         if choice == "":
             dg.roll()
             print(dg)
-            info = "Välj vilka träningar du vill behålla: "
+            info = "Välj vilka tärningar du vill behålla: "
             cap += 1
-        elif choice.lower() == 'q':
 
-            break
+    # När kastningen är klar: beräkna poäng
+values = dg.values()
+h = Score_calculator.histogram(values)
 
+print("\nPoäng:")
+for category, func in Score_calculator.fd.items():
+    print(f"{category}: {func(h)}")
