41aa145496daf05e3a02757b0982f1c5bff6c03c
Drairline <Daniil.airlend@gmail.com>
2026-03-16 14:19:01 +0100

    Update Scoreboard.py

--- Scoreboard.py
+++ Scoreboard.py
@@ -5,3 +5,20 @@ Created on Mon Mar  2 13:39:42 2026
 @author: 24daai01
 """
 
+scoreboard = {
+    "Ettor": None,
+    "Tvåor": None,
+    "Treor": None,
+    "Fyror": None,
+    "Femmor": None,
+    "Sexor": None,
+    "ett par": None,
+    "två par": None,
+    "Tretal": None,
+    "Fyrtal": None,
+    "Liten stege": None,
+    "Stor stege": None,
+    "Kåk": None,
+    "Chans": None,
+    "Yatzy": None
+}
