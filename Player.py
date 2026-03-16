2977d5a53150ba6d20ec02b4202f5f08f78fc39c
Drairline <Daniil.airlend@gmail.com>
2026-03-16 15:54:18 +0100

    Player.py

--- Player.py
+++ Player.py
@@ -5,3 +5,21 @@ Created on Mon Mar  2 13:40:07 2026
 @author: 24daai01
 """
 
+def player_maker():
+    while True:
+        try:
+            antal = int(input("Hur många spelare ska spela? "))
+            if antal > 0:
+                break
+            else:
+                print("Antal spelare måste vara minst 1.")
+        except ValueError:
+            print("Skriv ett heltal för antal spelare.")
+
+
+    player_list = []
+    for i in range(antal):
+        namn = input(f"Skriv namn på spelare {i+1}: ")
+        player_list.append({"namn": namn, "poang": 0})
+
+    return player_list
